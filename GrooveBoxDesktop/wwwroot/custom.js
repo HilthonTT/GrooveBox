21521ed30864e69c14e717d130bf5d7962517183
Timothy Tandt <Timothy.tandt@outlook.be>
2023-05-12 20:30:00 +0200

    Implemented MediaEdit.razor
    This page allows you to edit your media's title such as, title, description and its thumbnail but not the video on itself.

--- GrooveBoxDesktop/wwwroot/custom.js
+++ GrooveBoxDesktop/wwwroot/custom.js
@@ -4,3 +4,10 @@
         $(this).attr('title', fullTitle);
     });
 }
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+    myInput.focus()
+})
